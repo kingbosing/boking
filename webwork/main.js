cd15fbbb935fec1dff4650eeaad9ecc23e8dd4bd
王志博 <zhb_wang1@163.com>
2016-11-29 11:52:54 +0800

    1

--- webwork/main.js
+++ webwork/main.js
@@ -5,6 +5,7 @@
     var button0=document.getElementById('button0');
 
     button0.onclick=function () {
+    	console.log(1)
         var worker=new Worker('work.js');
         
         worker.postMessage('start')
--- webwork/work.js
+++ webwork/work.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2016/10/12 0012.
  */
 (function () {
-   function start() {
+   onmessage =function start() {
        for(var i=0;i<1000;i++){
            console.log(i)
        }
